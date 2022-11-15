4a4548f8309158feba0787b54b335451c00066e9
Furkanayaan <ayanf16@itu.edu.tr>
2022-11-15 12:28:06 +0300

    Initial commit

---
+++ TwoSets/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TwoSets
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Bir sayi giriniz");
+            string number = Console.ReadLine();
+            int N = Convert.ToInt32(number);
+            if (N % 4 == 1 || N % 4 == 2) Console.WriteLine("NO");
+            else if (N % 4 == 3)
+            {
+                Console.WriteLine("YES");
+                Console.WriteLine(N/2);
+                for (int i = 2; i <= N/2; i+=2)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                    Console.Write(N-i);
+                    Console.Write(" ");
+                }
+                
+                Console.Write(N);
+                Console.Write("\n");
+                Console.WriteLine(N/2+1);
+                for (int i = 1; i <= N/2; i+=2)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                    Console.Write(N-i);
+                    Console.Write(" ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("YES");
+                Console.WriteLine(N/2);
+                for (int i = 2; i <= N/2; i+=2)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                    Console.Write(N-i+1);
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+                Console.WriteLine(N/2);
+                for (int i = 1; i <= N/2; i+=2)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                    Console.Write(N-i+1);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
